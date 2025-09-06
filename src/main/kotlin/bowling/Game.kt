6d560531870198f285d8ef2a380177f9c5b73e21
kunwoo park <scv74502@gmail.com>
2025-09-06 17:57:17 +0900

    [refactor] refactor game test

--- src/main/kotlin/bowling/Game.kt
+++ src/main/kotlin/bowling/Game.kt
@@ -10,13 +10,31 @@ class Game {
 
     fun score(): Int {
         var score = 0
-        var i = 0
+        var frameIndex = 0
 
         for (frame in 0 until 10) {
-            score += rolls[i] + rolls[i + 1]
-            i += 2
+            if (isStrike(frameIndex)) {
+                score += 10 + strikeBonus(frameIndex)
+                frameIndex += 1
+            } else if (isSpare(frameIndex)) {
+                score += 10 + spareBonus(frameIndex)
+                frameIndex += 2
+            } else {
+                score += sumOfPinsInFrame(frameIndex)
+                frameIndex += 2
+            }
         }
 
         return score
     }
+
+    private fun isStrike(frameIndex: Int): Boolean = rolls[frameIndex] == 10
+
+    private fun strikeBonus(frameIndex: Int): Int = rolls[frameIndex + 1] + rolls[frameIndex + 2]
+
+    private fun spareBonus(frameIndex: Int): Int = rolls[frameIndex + 2]
+
+    private fun isSpare(frameIndex: Int): Boolean = rolls[frameIndex] + rolls[frameIndex + 1] == 10
+
+    private fun sumOfPinsInFrame(frameIndex: Int): Int = rolls[frameIndex] + rolls[frameIndex + 1]
 }
